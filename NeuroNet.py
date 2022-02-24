4069dd7bdad63cc5b5af83fbe6786be3c42daa39
mTerentev <termisha@mail.ru>
2022-02-24 20:49:44 +0300

    GPU boostup

--- NeuroNet.py
+++ NeuroNet.py
@@ -1,18 +1,17 @@
-import numpy as np
-import main
+import cupy as cp
 class Layer():
     values_vector=None
     previous_layer=None
     def __init__(self,n):
-        self.values_vector=np.zeros((n,1))
+        self.values_vector=cp.zeros((n,1))
     def connect(self,previous_layer):
         self.previous_layer=previous_layer
 
 class InputLayer(Layer):
     def __init__(self, n):
         super().__init__(n)
-    def set_input(self,i):
-        self.values_vector=np.transpose(np.array([i]))
+    def set_icput(self,i):
+        self.values_vector=cp.transpose(cp.array([i]))
 
 class OutLayer(Layer):
     def __init__(self):
@@ -21,7 +20,7 @@ class OutLayer(Layer):
         return self.previous_layer.values_vector
 
 class HiddenLayer(Layer):
-    activation_function=lambda self,x: 1/(1+np.exp(-x))
+    activation_function=lambda self,x: 1/(1+cp.exp(-x))
     activation_function_derivative=None
     delta_l=None
     offsets_vector=None
@@ -29,35 +28,35 @@ class HiddenLayer(Layer):
 
     def connect(self,previous_layer):
         super().connect(previous_layer)
-        self.weights_matrix=2*np.random.random((len(self.values_vector),len(previous_layer.values_vector)))-1
+        self.weights_matrix=2*cp.random.random((len(self.values_vector),len(previous_layer.values_vector)))-1
 
     def __init__(self, n):
         super().__init__(n)
-        self.offsets_vector=np.zeros((n,1))
+        self.offsets_vector=cp.zeros((n,1))
     
     def forward_propogate(self,learning=False):
         self.values_vector=self.activation_function(self.weights_matrix.dot(self.previous_layer.values_vector)+self.offsets_vector)
         if learning:
             self.activation_function_derivative=self.values_vector*(1-self.values_vector)
     def weights_update(self,a):
-        self.weights_matrix-=a*(self.delta_l.dot(np.transpose(self.previous_layer.values_vector)))
+        self.weights_matrix-=a*(self.delta_l.dot(cp.transpose(self.previous_layer.values_vector)))
     def offsets_update(self,a):
         self.offsets_vector-=a*self.delta_l
 
 class Net():
-    input_layer:InputLayer=None
+    icput_layer:InputLayer=None
     hidden_layers:HiddenLayer=[]
     out_layer:OutLayer=None
 
     def __init__(self,*args):
-        self.input_layer, *self.hidden_layers, self.out_layer=args
-        pl=self.input_layer
+        self.icput_layer, *self.hidden_layers, self.out_layer=args
+        pl=self.icput_layer
         for layer in [*self.hidden_layers, self.out_layer]:
             layer.connect(pl)
             pl=layer
     
-    def forward_propogate(self,input,learn=False):
-        self.input_layer.set_input(input)
+    def forward_propogate(self,icput,learn=False):
+        self.icput_layer.set_icput(icput)
         for layer in self.hidden_layers:
             layer.forward_propogate(learn)
         return self.out_layer.output()
@@ -70,13 +69,13 @@ class Net():
             return 0
         self.backward_propogate(error,i+1)
         next_layer=self.hidden_layers[i+1]
-        layer.delta_l=(np.transpose(next_layer.weights_matrix)).dot(next_layer.delta_l)*layer.activation_function_derivative
+        layer.delta_l=(cp.transpose(next_layer.weights_matrix)).dot(next_layer.delta_l)*layer.activation_function_derivative
 
     def train(self,x_train,y_train,alpha=0.01,batch=1,iterations=10000):
         for iteration in range(iterations):
             print(iteration)
-            k=np.random.randint(len(x_train))
-            target=np.zeros((10,1))
+            k=cp.random.randint(len(x_train))
+            target=cp.zeros((10,1))
             target[y_train[k]]=1
             error=self.forward_propogate(x_train[k])-target
             self.backward_propogate(error)
@@ -85,21 +84,19 @@ class Net():
                 layer.offsets_update(100*alpha)
 
     def print(self):
-        for layer in [self.input_layer,self.hidden_layers,self.out_layer]:
-            print(np.transpose(layer.values_vector))
+        for layer in [self.icput_layer,self.hidden_layers,self.out_layer]:
+            print(cp.transpose(layer.values_vector))
 
     def save_config(self,file="config.npz"):
         a=[]
         for layer in self.hidden_layers:
             a.append(layer.weights_matrix)
             a.append(layer.offsets_vector)
-        np.savez_compressed(file,*a)
+        cp.savez_compressed(file,*a)
     
     def load_config(self,file="config.npz"):
         i=0
         for layer in self.hidden_layers:
-            layer.weights_matrix=np.load(file)["arr_"+str(i)]
-            layer.offsets_vector=np.load(file)["arr_"+str(i+1)]
+            layer.weights_matrix=cp.load(file)["arr_"+str(i)]
+            layer.offsets_vector=cp.load(file)["arr_"+str(i+1)]
             i+=2
-
-main
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import numpy as np
 from NeuroNet import *
 import tensorflow as tf
-import numpy as np
+import cupy as cp
 import matplotlib.pyplot as plt
 
 def index_of_max(vector):
@@ -15,10 +16,16 @@ def index_of_max(vector):
     return index
 
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
-x_train, x_test = x_train[..., np.newaxis]/255.0, x_test[..., np.newaxis]/255.0
+x_train, x_test = x_train[..., cp.newaxis]/255.0, x_test[..., cp.newaxis]/255.0
 
-x_train_prep=np.reshape(x_train,(60000,28**2))
-x_test_prep=np.reshape(x_test,(10000,28**2))
+x_train_prep=cp.reshape(x_train,(60000,28**2))
+x_test_prep=cp.reshape(x_test,(10000,28**2))
+
+x_train_prep=cp.array(x_train_prep)
+x_test_prep=cp.array(x_test_prep)
+
+y_train=cp.array(y_train)
+y_test=cp.array(y_test)
 
 print("Number of original training examples:", len(x_train))
 print("Number of original test examples:", len(x_test))
@@ -45,6 +52,7 @@ for i in range(n):
     ax[0][i].set_yticklabels([])
     ax[0][i].set_xticklabels([])
     temp=net.forward_propogate(x_test_prep[k])
+    temp=cp.asnumpy(temp)
     temp.resize(10)
     ax[1][i].bar(np.arange(10),temp)
     ax[1][i].set_xticks(np.arange(10),labels=['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'])
@@ -55,11 +63,11 @@ result=0
 
 for i in range(100000):
     print(i/1000)
-    k=np.random.randint(10000)
+    k=cp.random.randint(10000)
     out=net.forward_propogate(x_test_prep[k])
     if index_of_max(out)==y_test[k]:
         result+=1
 result/=1000
 plt.show()
 print(result)
-# input()
+# icput()
